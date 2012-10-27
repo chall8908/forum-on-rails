f09dc435a7d8784f217aaa6d107ddcd21cdf4d54
Markus <chall8908@gmail.com>
2012-10-27 10:43:22 -0500

    Updated environment variables to use the correct name for the project

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Forum::Application.load_tasks
+ForumOnRails::Application.load_tasks
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Forum::Application.config.secret_token = 'fcab23748102084bc837465c9c29cbada962a294f1e087066ddd15bc2e7d6daa84b244a224e9c3b16cea2b4cfdf1be76404d9e5536f3ff3e1676b338312febdc'
+ForumOnRails::Application.config.secret_token = 'fcab23748102084bc837465c9c29cbada962a294f1e087066ddd15bc2e7d6daa84b244a224e9c3b16cea2b4cfdf1be76404d9e5536f3ff3e1676b338312febdc'
