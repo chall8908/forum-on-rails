0efcda67f712d25e50cd91ff0a3e6b8c1bed2f12
Markus <chall8908@gmail.com>
2012-11-06 22:04:04 -0600

    more typos....

--- app/models/encrypted_password.rb
+++ app/models/encrypted_password.rb
@@ -6,7 +6,7 @@ class EncryptedPassword < String
   
   def initialize(password, salt = nil)
     @salt = salt || encrypt(password, Time.now)
-    @password = encrrypt password, salt
+    @password = encrypt password, salt
   end
   
   def to_s
