26f805b6151c26ad509eed483673ed80c11b6787
Chris Hall <chall@pangeaequity.com>
2013-02-06 19:06:26 -0600

    Updated Bootstrap styles Finally have a working navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def user_is_admin?
     forem_user.andand.forem_admin
   end
+
+  def tab_link(value, path)
+    link_to value, path, class: "user-dashboard-tab #{request.path === path ? "active" : ""}"
+  end
 end
